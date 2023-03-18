3e09bf6d95b63f322905a6e78ed699b3d30f9ed6
vrmachado <vrmachado@gmail.com>
2023-03-18 14:13:46 +0100

    Fecha de alta agregada

--- src/main/java/com/example/entities/Borrar.java
+++ src/main/java/com/example/entities/Borrar.java
@@ -1,9 +1,12 @@
 package com.example.entities;
 
+import java.time.LocalDate;
+
 public class Borrar {
     private String name;
     private String primerApellido;
     private String segundoApellido;
+    private LocalDate fechaAlta;
 
     public enum Dias {
         LUNES, MARTES, MIERCOLES
