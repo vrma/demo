b554fe45331587cfd285be3d6cc63c1a5e677a73
vrmachado <vrmachado@gmail.com>
2023-03-18 14:15:10 +0100

    Fecha de Nacimiento agregada por aqui

--- src/main/java/com/example/entities/Borrar.java
+++ src/main/java/com/example/entities/Borrar.java
@@ -1,7 +1,10 @@
 package com.example.entities;
 
+import java.time.LocalDate;
+
 public class Borrar {
     private String name;
+    private LocalDate fechaDeNacimiento;
 
     public enum Dias {
         LUNES, MARTES, MIERCOLES
