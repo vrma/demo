a2b99730a2e143c6857fb7beda67bc1833c606ca
vrmachado <vrmachado@gmail.com>
2023-03-18 13:12:41 +0100

    Bien con Stash

--- src/main/java/com/example/entities/Borrar.java
+++ src/main/java/com/example/entities/Borrar.java
@@ -2,6 +2,8 @@ package com.example.entities;
 
 public class Borrar {
     private String name;
+    private String primerApellido;
+    private String segundoApellido;
 
     public enum Dias {
         LUNES, MARTES, MIERCOLES
